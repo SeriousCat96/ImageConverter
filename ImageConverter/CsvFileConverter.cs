fd2054c75d63cf9532a6b07df20eec4e432959d2
Pavel Shevelev <thisisbusiness@yandex.ru>
2017-07-03 14:44:42 +0300

    wip

---
+++ ImageConverter/CsvFileConverter.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using OpenCvSharp;
+using OpenCvSharp.CPlusPlus;
+
+namespace ImageConverter
+{
+	public class CsvFileConverter
+	{
+		public const string RootImageFolderName = "images";
+		public const string RootOutputFolderName = "out";
+		
+		public CsvFileConverter(string fileExtension)
+		{
+			ImageCounter = 0;
+			FileExtension = fileExtension;
+		}
+
+		public string FileExtension { get; }
+		public int ImageCounter { get; private set; }
+
+		public void Convert(string fullFileName)
+		{
+			var fileInfo = new FileInfo(fullFileName);
+
+			if(fileInfo.Extension != FileExtension) return;
+
+			var bitmap = new Bitmap(fullFileName);
+
+			var folders = Regex
+				.Match(fullFileName, $@"{RootImageFolderName}.*")
+				.Value;
+
+			var dirName = folders.Substring(RootImageFolderName.Length + 1);
+
+			var d = Path.Combine(Environment.CurrentDirectory, RootOutputFolderName, dirName);
+			var outputDir = Directory
+				.GetParent(d)
+				.FullName;
+
+			if(!Directory.Exists(outputDir))
+			{
+				Directory.CreateDirectory(outputDir);
+			}
+
+			var outputPath = GetCsvFilePath(outputDir);
+
+			if(File.Exists(outputPath)) return;
+
+			AppendText(outputPath, "X,Y,R,G,B");
+
+			for(int x = 0; x < bitmap.Width; ++x)
+			{
+				for(int y = 0; y < bitmap.Height; ++y)
+				{
+					var pixel = bitmap.GetPixel(x, y);
+
+					AppendText(outputPath, $"{x},{y},{pixel.R},{pixel.G},{pixel.B}");
+				}
+			}
+		}
+
+		public string GetCsvFilePath(string directory)
+		{
+			return Path.Combine(directory, (ImageCounter++).ToString() + ".csv");
+		}
+
+		public void AppendText(string filename, string text)
+		{
+			using(var writer = new StreamWriter(filename, true, Encoding.Default))
+			{
+				writer.WriteLine(text);
+				writer.Flush();
+			}
+		}
+	}
+}
---
+++ ImageConverter/MainForm.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.CPlusPlus;
+
+namespace ImageConverter
+{
+	public partial class MainForm : Form
+	{
+		private const string FileExtension = ".jpeg";
+
+		private CsvFileConverter _csvFileConverter;
+		private ImageResizer _imageResizer;
+
+		public MainForm()
+		{
+			InitializeComponent();
+			_csvFileConverter = new CsvFileConverter(FileExtension);
+			_imageResizer = new ImageResizer();
+		}
+
+		private void ConvertFiles()
+		{
+			var rootDir = Path.Combine(Directory.GetCurrentDirectory(), CsvFileConverter.RootImageFolderName);
+			Convert(rootDir);
+		}
+
+		private void Convert(string path)
+		{
+			var directories = Directory.EnumerateDirectories(path);
+
+			if(directories.Count() != 0)
+			{
+				foreach(var directory in directories)
+				{
+					Convert(directory);
+				}
+			}
+			else
+			{
+				var fullFileNames = Directory
+					.EnumerateFiles(path)
+					.Where(e => e.EndsWith(FileExtension));
+
+
+				foreach(var fullFileName in fullFileNames)
+				{
+					
+					_imageResizer.Resize(fullFileName);
+					_csvFileConverter.Convert(fullFileName);
+				}
+			}
+		}
+
+		private void OnButtonConvertClick(object sender, EventArgs e)
+		{
+			ConvertFiles();
+		}
+	}
+}
